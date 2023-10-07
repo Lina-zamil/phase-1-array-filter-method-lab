50d49251f70e8e1dc68752f62f0f8e6e0ab6343c
lina abumahfood <lina.abumahfood@gmail.com>
2023-10-07 23:03:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(arr, string) {
+    let findMatchingResult = arr.filter(el => el.toUpperCase() === string.toUpperCase())
+    return findMatchingResult
+}
+console.log((findMatching(drivers, 'bobby')))
+
+
+function fuzzyMatch(arr, string) {
+    let fuzzyMatchResult = arr.filter(el => el.startsWith(string))
+    return fuzzyMatchResult
+}
+console.log((fuzzyMatch(drivers, 'Sa')))
+
+let driverss = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+function matchName(arr, string) {
+    let matchNameResult = arr.filter(el => el.name.toLowerCase() === string.toLowerCase())
+    return matchNameResult
+}
+console.log((matchName(driverss, 'bobby')))
+
